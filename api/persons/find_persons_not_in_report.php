a5140c6c3d26fcd30e2271488e4146639eee580b
Wiwit Aditya <wiwit.aditya.saputra@gmail.com>
2016-12-13 00:19:38 +0700

    Rename column from is_active to state

--- api/persons/find_persons_not_in_report.php
+++ api/persons/find_persons_not_in_report.php
@@ -1,7 +1,7 @@
 <?php
 include '../connect.php';
 
-$sql = "select p.* from pelanggan p where p.id not in (select r.id_person from reports_test_results r where r.is_active = 1)";
+$sql = "select p.* from pelanggan p where p.id not in (select r.id_person from reports_test_results r where r.state = 1)";
 $result = $conn->query($sql);
 $rows = array();
 if ($result->num_rows > 0) {
